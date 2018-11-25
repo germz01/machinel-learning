adfc617d0d1ae77036ffbf895f72676cbb553e7d
Stefano Carpita <carpitastefano@gmail.com>
2018-11-25 20:34:17 +0100

    updated backprop

--- scripts/testing.py
+++ scripts/testing.py
@@ -15,12 +15,23 @@ imp.reload(utils)
 
 nn = neural_network.NeuralNetwork(hidden_sizes=(100, 3, 2),
                                   activation='sigmoid', max_epochs=500)
-nn.train(X, y, eta=2, alpha=0.7)
+nn.train(X, y, eta=0.5, alpha=0.7)
 nn.y_pred
+
+
+for iy in nn.delta:
+        print iy.shape
+
+for delta_W in nn.delta_W:
+        print delta_W.shape
+
+for W in nn.W:
+        print W.shape        
+        
+        
 
 
 if raw_input('\nDO YOU WANT TO PLOT THE LEARNING CURVE?[Y/N] ') == 'Y':
         utils.plot_learning_curve(nn.empirical_risk, nn.max_epochs)
 
 # ann prediction
-
