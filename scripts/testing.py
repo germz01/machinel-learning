3716c636d43167d206864099b32d2a916d959e39
Stefano Carpita <carpitastefano@gmail.com>
2018-12-15 14:07:59 +0100

    topology and some inits in train

--- scripts/testing.py
+++ scripts/testing.py
@@ -1,112 +1,35 @@
+import nn as NN
 import numpy as np
-import utils
 import matplotlib.pyplot as plt
-import neural_network
 import imp
+import utils as u
 
 X = np.concatenate((np.random.normal(2., 1., (3, 2)),
                     np.random.normal(5., 1., (2, 2))),
                    axis=0)
-
-# Y = X[]
-# y = np.round((2*X[:, 0]+5*X[:, 1]+10*X[:,0]*X[:,1])/20)
-y = np.array([1, 1, 1, -1, -1])
+y = np.array([1, 1, 1, 0, 0]).reshape(5, 1)
 
 
-# reloading del module per sviluppo
-imp.reload(neural_network)
-imp.reload(utils)
-
-momentum = raw_input('MOMENTUM[classic/nesterov]: ')
-
-nn = neural_network.NeuralNetwork(hidden_sizes=(20, 5),
-                                  activation=['sigmoid'], max_epochs=1000,
-                                  max_weight_init=0.7)
-
-nn.train(X, y, eta=0.2, momentum=momentum, alpha=0.7)
-
-np.round(nn.y_pred, 2)
-
-utils.plot_learning_curve([nn.empirical_risk, nn.error_rmse, nn.error_mee,
-                          np.array(nn.error_mee)+np.array(nn.error_mee_dev)],
-                          nn.max_epochs, momentum=momentum,
-                          fname=['empirical_risk_', 'error_rmse_',
-                          'error_mee_', 'error_mee_dev_'])
 
 
-# plt.plot(range(nn.max_epochs), np.array(nn.error_mee)+np.
-#          array(nn.error_mee_dev))
-# plt.plot(range(nn.max_epochs), np.array(nn.error_mee)-np.
-#          array(nn.error_mee_dev))
-# plt.plot(range(nn.max_epochs), np.array(nn.error_mee))
-# plt.show()
+imp.reload(NN)
+imp.reload(u)
 
-# plt.plot(range(nn.max_epochs), np.array(nn.error_mee_dev))
-# plt.plot(range(nn.max_epochs), -np.array(nn.error_mee_dev))
-# plt.show()
+nn = NN.NeuralNetwork(hidden_sizes = [10 ] )
 
+eta = .1
+alpha = .9
+nn.train(X, y, eta, alpha, [0.01, 'l2'], 1000)
 
-for iy in nn.Y:
-        print iy.shape
-
-for d in nn.delta:
-        print d.shape
-
-for delta_W in nn.delta_W:
-        print delta_W.shape
-
-for W in nn.W:
-        print W.shape
+nn.topology
 
 
-# if raw_input('\nDO YOU WANT TO PLOT THE LEARNING CURVE?[Y/N] ') == 'Y':
-#        utils.plot_learning_curve(nn.empirical_risk, nn.max_epochs)
+epochs = 500
+# nota: loss online e epochs sono su range diversi
+# da sistemare successivamente a batch/minibatch/online implementation
+fig, ax = plt.subplots(figsize = (15,7))
+plt.plot(range(len(nn.loss_online[:epochs])), nn.loss_online[:epochs])
+plt.plot(range(len(nn.loss_epochs[:epochs])), nn.loss_epochs[:epochs])
 
-
-###########################################################
-
-# testing error functions
-
-p = 4
-k = 1
-
-# d = np.ones((p,k)).reshape((p,))
-
-d = np.arange(p*k).reshape((p, 1))
-y = np.ones((p, k))
-
-d
-y
-
-# rmse
-
-print np.sqrt(np.einsum('pk->', (d-y)**2) / p)
-
-# mean euclidean error
-print np.einsum('p->', np.sqrt(np.einsum('pk->p', (d-y)**2))) / p
-
-
-def rmse(d, y):
-        '''root mean square error'''
-        p = d.shape[0]
-        if len(d.shape) == 1:
-                d = d.reshape((p, 1))
-        if len(y.shape) == 1:
-               y = y.reshape((p, 1))
-
-        return np.sqrt(np.einsum('pk->', (d-y)**2) / p)
-
-
-def mee(d, y):
-        ''' mean euclidean error'''
-        p = d.shape[0]
-        if len(d.shape) == 1:
-                d = d.reshape((p, 1))
-        if len(y.shape) == 1:
-               y = y.reshape((p, 1))
-
-        return np.einsum('p->', np.sqrt(np.einsum('pk->p', (d-y)**2))) / p
-
-
-# y = np.round(np.random.uniform(-1,1, (p,k)),1)
-
+plt.savefig('../images/temp_loss.pdf')
+plt.close()
