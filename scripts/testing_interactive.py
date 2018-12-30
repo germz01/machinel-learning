77736ae520695cc9065bc4cf6675641196ea3fe5
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-12-30 16:11:10 +0100

    Updated testing_interactive script

--- scripts/testing_interactive.py
+++ scripts/testing_interactive.py
@@ -1,8 +1,5 @@
-import nn as NN
+import nn
 import numpy as np
-import matplotlib.pyplot as plt
-import imp
-import utils as u
 import validation as val
 
 X = np.concatenate((np.random.normal(2., 1., (50, 2)),
@@ -10,27 +7,16 @@ X = np.concatenate((np.random.normal(2., 1., (50, 2)),
                    axis=0)
 y = np.hstack((np.ones(50), np.zeros(50))).reshape(100, 1)
 
-
-imp.reload(NN)
-imp.reload(u)
-
 eta = .1
 alpha = .9
 
 if raw_input('PLAIN EXECUTION WITHOUT TESTING?[Y/N] ') == 'Y':
-    nn = NN.NeuralNetwork(X.shape[1], [10], 1)
-    nn.train(X, y, eta, alpha, [0.01, 'l2'], 1000)
+    neural_net = nn.NeuralNetwork(X, y)
+    neural_net.train(X, y, eta, alpha=alpha, epochs=500, batch_size=10,
+                     reg_lambda=0.001, reg_method='l2')
 
     if raw_input('PLOT LEARNING CURVE?[Y/N] ') == 'Y':
-        epochs = 500
-        # nota: loss online e epochs sono su range diversi
-        # da sistemare successivamente a batch/minibatch/online implementation
-        fig, ax = plt.subplots(figsize=(5, 7))
-        plt.plot(range(len(nn.loss_online[:epochs])), nn.loss_online[:epochs])
-        plt.plot(range(len(nn.loss_epochs[:epochs])), nn.loss_epochs[:epochs])
-
-        plt.savefig('../images/temp_loss.pdf')
-        plt.close()
+        pass
 
 if raw_input('TESTING K-FOLD CROSS VALIDATION?[Y/N] ') == 'Y':
     neural_net = nn.NeuralNetwork(X, y)
